0acb65d246f524b8465b953dc35c241d5a1e4651
johs <johs@cresoty.co.kr>
2026-03-26 13:40:08 +0900

    업데이트 test    versionCode = 4         versionName = "1.0.1"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.cresoty.catpospoin"
         minSdk = 34
         targetSdk = 34
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0.1"
 
         testInstrumentationRunner = "com.cresoty.catpospoin.HiltTestRunner"
