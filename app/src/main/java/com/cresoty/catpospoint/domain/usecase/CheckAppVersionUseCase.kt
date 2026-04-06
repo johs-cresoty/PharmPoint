275fd8fba8f13508877b7f3355d2af809409a8d2
johs <johs@cresoty.co.kr>
2026-04-06 14:02:24 +0900

    feat: 환경설정 업데이트 기능 추가

---
+++ app/src/main/java/com/cresoty/catpospoint/domain/usecase/CheckAppVersionUseCase.kt
@@ -0,0 +1,13 @@
+package com.cresoty.catpospoint.domain.usecase
+
+import com.cresoty.catpospoint.dataresource.DataResource
+import com.cresoty.catpospoint.domain.model.UpdateInfo
+import com.cresoty.catpospoint.domain.repository.UpdateRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class CheckAppVersionUseCase @Inject constructor(
+    private val repo: UpdateRepository,
+) {
+    operator fun invoke(): Flow<DataResource<UpdateInfo>> = repo.checkForUpdate()
+}
