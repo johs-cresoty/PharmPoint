191fbc83a2b90140efdbec22d93d6bf9d23a592c
johs <johs@cresoty.co.kr>
2026-05-20 14:58:04 +0900

    API 설명 주석 추가

--- app/src/main/java/com/cresoty/catpospoint/remote/api/CatposCloudApi.kt
+++ app/src/main/java/com/cresoty/catpospoint/remote/api/CatposCloudApi.kt
@@ -15,6 +15,9 @@ import retrofit2.http.Query
 
 interface CatposCloudApi {
 
+    /**
+     * 포인트 적립 설정 정보 조회
+     */
     @GET("api/point/settings")
     suspend fun getPointSaveSetting(
         @Query("TAXNO") taxNo: String,
@@ -23,6 +26,9 @@ interface CatposCloudApi {
         @Query("POS_GUBN") posGubn: String
     ): PointSaveSettingResponse
 
+    /**
+     * 결제 금액별 포인트 설정 목록 조회
+     */
     @GET("api/point/payment-settings")
     suspend fun getPointAmountSetting(
         @Query("TAXNO") taxNo: String,
@@ -31,6 +37,9 @@ interface CatposCloudApi {
         @Query("POS_GUBN") posGubn: String
     ): PointAmountSettingResponse
 
+    /**
+     * 고객 목록 조회
+     */
     @GET("/api/terminals/customers")
     suspend fun getCustomer(
         @Query("CMPTR_NAME") computerName: String,
@@ -39,6 +48,9 @@ interface CatposCloudApi {
         @Query("CST_HP") customerHp: String
     ): CustomerResponse
 
+    /**
+     * 고객 코드 조회
+     */
     @GET("api/terminals/customers/code")
     suspend fun getPointBalance(
         @Query("TAXNO") taxNo: String,
@@ -48,12 +60,18 @@ interface CatposCloudApi {
         @Query("POS_GUBN") posGubn: String
     ): PointBalanceResponse
 
+    /**
+     *  적립 예상 포인트 조회
+     */
     @POST("api/point/estimate")
     suspend fun estimatePoint(
         @Body request: EstimatePointRequest
     ): EstimatePointResponse
 
 
+    /**
+     * 고객 판매정보 갱신 및 고객코드 조회
+     */
     @POST("/api/terminals/customers/code")
     suspend fun upsertCustomerPoint(
         @Body request: UpsertCustomerPointRequest
